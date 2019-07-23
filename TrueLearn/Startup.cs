0185e7ca887ba79b6e7ef21f5092109fd7541e6f
τασος σισμανογλου <tasossisman@gmail.com>
2019-07-23 10:46:23 +0300

    Added roles (Admin, FreeUser, PremiumUser) and created admin user (name: admin, password: iamtheadmin)

--- TrueLearn/Startup.cs
+++ TrueLearn/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using TrueLearn.Models;
 
 [assembly: OwinStartupAttribute(typeof(TrueLearn.Startup))]
 namespace TrueLearn
@@ -9,6 +12,50 @@ namespace TrueLearn
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+        }
+
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "admin";
+                user.Email = "admin@gmail.com";
+                //user.birth_date = new DateTime(2000, 1, 1);
+                //user.country = "Greece";
+                user.first_name = "admin";
+                user.last_name = "admin";
+                string userPWD = "iamtheadmin";
+                var chkUser = UserManager.Create(user, userPWD);
+
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("PremiumUser"))
+            {
+                var role = new IdentityRole();
+                role.Name = "PremiumUser";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("FreeUser"))
+            {
+                var role = new IdentityRole();
+                role.Name = "FreeUser";
+                roleManager.Create(role);
+            }
         }
     }
 }
