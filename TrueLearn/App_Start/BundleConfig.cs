ed4b6b41146c3958afd46f595fc7ea84a1295da9
Katri Tzi <katri.cullen@gmail.com>
2019-07-26 16:24:53 +0300

    LAYOUT BITCHES

--- TrueLearn/App_Start/BundleConfig.cs
+++ TrueLearn/App_Start/BundleConfig.cs
@@ -24,7 +24,9 @@ namespace TrueLearn
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/styles.css"));
+
         }
     }
 }
