b875140397122e48336cbcfc8631c651a29b4300
τασος σισμανογλου <tasossisman@gmail.com>
2019-08-02 13:25:07 +0300

    commit before push

--- TrueLearn/Controllers/HomeController.cs
+++ TrueLearn/Controllers/HomeController.cs
@@ -1,30 +1,56 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using TrueLearn.Managers;
+using TrueLearn.Models;
+using TrueLearn.ViewModels;
 
 namespace TrueLearn.Controllers
 {
     public class HomeController : Controller
     {
+        private DbManager db = new DbManager();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public JsonResult GetEvents()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            
+                var events = db.GetTodoTasks();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult Create(TaskViewModel vm)
+        {
+            
+            vm.TodoTask.UserId = User.Identity.GetUserId();
+            db.AddTodoTask(vm.TodoTask, vm.SelectedCourses);
+            return new JsonResult { Data = new { vm.TodoTask, vm.SelectedCourses } };
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public JsonResult DeleteConfirmed(int id)
+        {
+            var status = false;
+            var task = db.GetTodoTask(id);
+            if (task != null)
+            {
+                db.DeleteTodoTask(id);
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
         }
     }
 }
