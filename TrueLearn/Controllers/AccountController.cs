9158c3658ee8744811341f87e7893ce3d183bf06
Giorgos Rouselatos <Giorgos Rouselatos@LAPTOP-J5OLM510>
2019-08-04 20:36:57 +0300

    Edited the logoff action

--- TrueLearn/Controllers/AccountController.cs
+++ TrueLearn/Controllers/AccountController.cs
@@ -396,7 +396,7 @@ namespace TrueLearn.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Login", "Home");
+            return RedirectToAction("Index", "Home");
         }
 
         //
