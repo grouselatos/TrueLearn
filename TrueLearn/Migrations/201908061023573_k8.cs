742a080087c9bd115e66fa0c9bc5411a60ba5ed4
Katri Tzi <katri.cullen@gmail.com>
2019-08-06 14:54:33 +0300

    Dashboard UI

---
+++ TrueLearn/Migrations/201908061023573_k8.cs
@@ -0,0 +1,16 @@
+namespace TrueLearn.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class k8 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
